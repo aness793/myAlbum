a51f1c49b6f662f5eb00eb55ef535525a5ad186b
Aness Rahmani <d.rahmanianess@gmail.com>
2024-06-23 20:27:38 +0100

    responsivnees

--- dist/gallery/server/main.server.mjs
+++ dist/gallery/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DSBBGSMF.mjs";import"./chunk-7ZHK63YM.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-FS2G4SVV.mjs";import"./chunk-7ZHK63YM.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
